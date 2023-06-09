21f2ffa7ae8dbffe754a9123bb9da2b1fb516964
stanislav <stanislavmuratov@yandex.ru>
2023-06-09 23:36:15 +0300

    add second execise

--- homework2.java
+++ homework2.java
@@ -3,7 +3,38 @@ import java.util.Scanner;
 
 public class homework2 {
     public static void main(String[] args) {
-        System.out.println(inputFloat());
+        // ** 1 
+        // System.out.println(inputFloat());
+
+        // ** 2 Испраить код (1)
+        int[] intArray = new int[9];
+        try {
+            int d = 0;
+            // double doubleArr = 1;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+         } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+         }
+        // ** 2 Испраить код (2)
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (NullPointerException ex) {
+            System.out.println("Указатель не может указывать на null!");
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        }
+        catch (Throwable ex) {
+            System.out.println("Что-то пошло не так...");
+        }
+      
+         
+         
     }
 
     // **1 Метод, который запрашивает у пользователя ввод дробного числа
@@ -19,5 +50,13 @@ public class homework2 {
             System.out.println("Некорректный ввод, попробуйте снова ...");
             return inputFloat();
         }
+        finally{
+            sc.close();
+        }
     }
+    
+    public static void printSum(Integer a, Integer b) throws Exception {
+        System.out.println(a + b);
+     }
+     
 }
